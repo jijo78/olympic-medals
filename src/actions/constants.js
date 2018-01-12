8abd5c1383ad191dc2ec09de51f4456bf7a1cb23
Giuseppe de Marino <giuche@gmail.com>
2018-01-12 18:16:29 +0000

    initial commit

--- src/actions/constants.js
+++ src/actions/constants.js
@@ -3,9 +3,5 @@ export const SORT_GOLD = 'SORT_GOLD';
 export const SORT_SILVER = 'SORT_SILVER';
 export const SORT_BRONZE = 'SORT_BRONZE';
 export const SORT_TOTAL = 'SORT_TOTAL';
-export const TOTAL = 'TOTAL';
 
-
-
-export const FETCH_ERROR = 'FETCH_ERROR';
 
